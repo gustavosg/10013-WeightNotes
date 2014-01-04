e297f03819c91e749861582a5e2205029640ebec
Gustavo Souza Gonçalves <gustavosouzagoncalves@gmail.com>
2014-01-04 19:14:06 -0200

    Ajustes

--- Programa/Sistema/WeightNotes/Views/Food/FoodsView.xaml.cs
+++ Programa/Sistema/WeightNotes/Views/Food/FoodsView.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
 
 namespace WeightNotes.Views.Food
 {
@@ -18,6 +19,63 @@ namespace WeightNotes.Views.Food
         public FoodsView()
         {
             InitializeComponent();
+
+            ChangeApplicationBar(false);
+        }
+
+        private void CheckBox_Clicked(object sender, RoutedEventArgs e)
+        {
+            Boolean value = ((CheckBox)sender).IsChecked == true ? true : false;
+
+            ChangeApplicationBar(value);
+
+        }
+
+        /// <summary>
+        /// Change in Runtime the Application Bar
+        /// </summary>
+        /// <param name="value">If there's any checkbox checked</param>
+        private void ChangeApplicationBar(Boolean value)
+        {
+            ApplicationBar appBar = new ApplicationBar();
+
+            if (value)
+            {
+                ApplicationBarIconButton iconButton1 = new ApplicationBarIconButton();
+                iconButton1.Text = "editar";
+                iconButton1.IconUri = new Uri("Images/dark/appbar.edit.rest.png", UriKind.Relative);
+
+                appBar.Buttons.Add(iconButton1);
+
+                ApplicationBarIconButton iconButton2 = new ApplicationBarIconButton();
+                iconButton2.Text = "excluir";
+                iconButton2.IconUri = new Uri("Images/dark/appbar.delete.rest.png", UriKind.Relative);
+                appBar.Buttons.Add(iconButton2);
+            }
+
+            else
+            {
+                ApplicationBarIconButton iconButton1 = new ApplicationBarIconButton();
+                iconButton1.Text = "novo";
+                iconButton1.IconUri = new Uri("Images/dark/appbar.new.rest.png", UriKind.Relative);
+
+                appBar.Buttons.Add(iconButton1);
+
+                ApplicationBarIconButton iconButton2 = new ApplicationBarIconButton();
+                iconButton2.Text = "procurar";
+                iconButton2.IconUri = new Uri("Images/dark/appbar.feature.search.rest.png", UriKind.Relative);
+                appBar.Buttons.Add(iconButton2);
+
+                //ApplicationBarIconButton iconButton3 = new ApplicationBarIconButton();
+                //iconButton3.Text = "filtrar";
+                //iconButton3.IconUri = new Uri("Images/dark/appbar.filter.png", UriKind.Relative);
+                //appBar.Buttons.Add(iconButton3);
+
+                
+            }
+
+            FoodsViewPage.ApplicationBar = appBar;
+            
         }
     }
 }
